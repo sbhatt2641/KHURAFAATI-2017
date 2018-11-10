596e91714f7c65e30ea2e7c210a309a8e610bab4
Sudhir Bhatt <sudhir.bhatt@charter.com>
2018-11-10 06:14:04 -0600

    Integral commit.

--- api-resource/src/main/java/com/maxopus/cloud/resourceserver/WebSecurityConfig.java
+++ api-resource/src/main/java/com/maxopus/cloud/resourceserver/WebSecurityConfig.java
@@ -3,16 +3,14 @@ package com.maxopus.cloud.resourceserver;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	/*@Override
